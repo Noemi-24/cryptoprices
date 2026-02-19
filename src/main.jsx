b4335ece91916da74d1b665250cfe1740bd59ced
Noemi Roldan <noemi.roldan@peopleshores.com>
2026-02-19 10:28:20 -0500

    Setting Up the Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom";
 import './index.css'
 import App from './App.jsx'
+// Import BrowserRouter and rename it to Router
+import { BrowserRouter as Router } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+// Wrap the App component with the Router component to enable the router features.
+ReactDOM.render(
   <StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </StrictMode>,
-)
+  document.getElementById("root")
+);
