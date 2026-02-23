d11ce1869ead1fd68453a9d14e68dfa2e5aaeeb5
Noemi-24 <noemi.roldan@peopleshores.com>
2026-02-23 15:53:20 -0500

    Change API to CoinGecko

--- src/pages/Currencies.jsx
+++ src/pages/Currencies.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function Currencies (props) {
   const currencies = [
-    { name: "Bitcoin", symbol: "BTC" },
-    { name: "Litecoin", symbol: "LTC" },
-    { name: "Ethereum", symbol: "ETH" },
-    { name: "Ethereum Classic", symbol: "ETC" },
-    { name: "Stellar Lumens", symbol: "XLM" },
-    { name: "Dash", symbol: "DASH" },
-    { name: "Ripple", symbol: "XRP" },
-    { name: "Zcash", symbol: "ZEC" },
+    { name: "Bitcoin", symbol: "bitcoin" },
+    { name: "Litecoin", symbol: "litecoin" },
+    { name: "Ethereum", symbol: "ethereum" },
+    { name: "Ethereum Classic", symbol: "ethereum-classic" },
+    { name: "Stellar Lumens", symbol: "stellar" },
+    { name: "Dash", symbol: "dash" },
+    { name: "Ripple", symbol: "ripple" },
+    { name: "Zcash", symbol: "zcash" },
   ];
 
   return (
--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -2,21 +2,33 @@ import {useState, useEffect} from "react";
 import {useParams} from "react-router-dom"
 
 export default function Price (props) {
-  // Our api key from coinapi.io.
-  const apiKey = "5c67521c-df27-45dc-9f16-5b9ee81c880a";
   // Grabbing the currency symbol from the URL Params.
   const params = useParams()
   const symbol = params.symbol
   // Using the other two variables to create our URL.
-  const url = `http://rest-sandbox.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
+  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${symbol}&vs_currencies=usd`;
 
+  const coinNames = {
+    "bitcoin": "Bitcoin",
+    "litecoin": "Litecoin",
+    "ethereum": "Ethereum", 
+    "ethereum-classic": "Ethereum Classic", 
+    "stellar": "Stellar Lumens", 
+    "dash": "Dash", 
+    "ripple": "Ripple", 
+    "zcash": "Zcash",  
+  }
   // State to hold the coin data.
   const [coin, setCoin] = useState("null");
 
   // Function to fetch coin data.
   const getCoin = async () => {
     try {
-      const response = await fetch(url);
+      const response = await fetch(url,{
+        headers: {
+          "x-cg-demo-api-key": "CG-TDhqU54CDCskomn4pbf2c3gi"
+        }
+      });
       const data = await response.json();
       setCoin(data);
     } catch(e) {
@@ -34,9 +46,9 @@ export default function Price (props) {
     return (
       <div>
         <h1>
-          {coin.asset_id_base}/{coin.asset_id_quote}
+          {coinNames[symbol]}/USD
         </h1>
-        <h2>{coin.rate}</h2>
+        <h2>{coin[symbol]?.usd}</h2>
       </div>
     );
   };
@@ -47,5 +59,5 @@ export default function Price (props) {
   };
 
   // If coin has data, run the loaded function; otherwise, run loading.
-  return coin && coin.rate ? loaded() : loading();
+  return coin && coin[symbol]?.usd ? loaded() : loading();
 }
